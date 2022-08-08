e0b0455bb5d4338262a9c20a8426e4fa54128ab6
Utshaan <Utkarsh.thanvi@gmail.com>
2022-08-09 03:37:24 +0530

    Formatter main.py with black

--- src/main.py
+++ src/main.py
@@ -33,45 +33,83 @@ async def tasklist(
         else:
             await ctx.send(compile_format(db.get_team_tasks(group_name)))
 
+
 @bot.command()
 @interactions.option(name="task_name", description="Name for the task", required=True)
 @interactions.option(name="description", description="Describe the task", required=True)
-@interactions.option(name="team_name", description="Team to add task to (leave empty for user tasks)", required=False)
-@interactions.option(name="assignees", description="Name of the assignees you'd like to add (Case sensitive, separate using ':')", required=False)
-@interactions.option(name="due_date", description="Due Date for the task (Year/Month/Date/Hour/Minute/Second)", required=False)
-@interactions.option(name="notes", description="Additional notes for the task(use | to separate notes)", required=False)
-async def new_task(ctx: interactions.CommandContext, task_name: str, description: str ,team_name: str = ' ', assignees: str = ' ', due_date: str = ' ', notes: str = ' '):
-    due_date = datetime(*tuple(map(lambda x : int(x), due_date.split('/')))) if due_date != ' ' else datetime(1,1,1)
-    notes = notes.split('|') if notes != ' ' else ["No notes"]
-    if team_name == ' ':
+@interactions.option(
+    name="team_name",
+    description="Team to add task to (leave empty for user tasks)",
+    required=False,
+)
+@interactions.option(
+    name="assignees",
+    description="Name of the assignees you'd like to add (Case sensitive, separate using ':')",
+    required=False,
+)
+@interactions.option(
+    name="due_date",
+    description="Due Date for the task (Year/Month/Date/Hour/Minute/Second)",
+    required=False,
+)
+@interactions.option(
+    name="notes",
+    description="Additional notes for the task(use | to separate notes)",
+    required=False,
+)
+async def new_task(
+    ctx: interactions.CommandContext,
+    task_name: str,
+    description: str,
+    team_name: str = " ",
+    assignees: str = " ",
+    due_date: str = " ",
+    notes: str = " ",
+):
+    due_date = (
+        datetime(*tuple(map(lambda x: int(x), due_date.split("/"))))
+        if due_date != " "
+        else datetime(1, 1, 1)
+    )
+    notes = notes.split("|") if notes != " " else ["No notes"]
+    if team_name == " ":
         task = Task(
-                task_name,
-                description,
-                ctx.author.name,
-                due_date=due_date,
-                notes=notes
-            )
+            task_name, description, ctx.author.name, due_date=due_date, notes=notes
+        )
         db.add_user_task(task)
-        await ctx.send(f"Task Added. Information about task :{compile_format([task.format()])}")
+        await ctx.send(
+            f"Task Added. Information about task :{compile_format([task.format()])}"
+        )
     else:
-        assignees = assignees.split(':') if assignees == ' ' else ["No assignees"]
+        assignees = assignees.split(":") if assignees == " " else ["No assignees"]
         task = TeamTask(
             task_name,
             team_name,
             description,
-            ctx.author.name,  
+            ctx.author.name,
             assignees,
             due_date=due_date,
-            notes=notes
+            notes=notes,
         )
         db.add_team_task(TeamTask)
-        await ctx.send(f"Task Added. Information about task : {compile_format([task.format()])}")
+        await ctx.send(
+            f"Task Added. Information about task : {compile_format([task.format()])}"
+        )
+
 
 @bot.command()
-@interactions.option(name="task_name", description="Name of the task to be deleted", required=True)
-@interactions.option(name="team_name", description="Team to add task to (leave empty for user tasks)", required=False)
-async def remove_task(ctx: interactions.CommandContext, task_name: str, team_name: str = ' '):
-    if team_name == ' ':
+@interactions.option(
+    name="task_name", description="Name of the task to be deleted", required=True
+)
+@interactions.option(
+    name="team_name",
+    description="Team to add task to (leave empty for user tasks)",
+    required=False,
+)
+async def remove_task(
+    ctx: interactions.CommandContext, task_name: str, team_name: str = " "
+):
+    if team_name == " ":
         db.delete_user_task_explicit(ctx.author.name, task_name)
         await ctx.send("`Task deleted`")
     else:
