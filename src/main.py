4c8a4c60a208621a6f9fba26c743ede4687ede4f
PraneethJain <praneethjain005@gmail.com>
2022-08-08 17:22:13 +0530

    Cleaned up code

--- src/main.py
+++ src/main.py
@@ -1,22 +1,9 @@
-from rich import print
 import interactions
 from auth import get_token
-
-# from db import teams, users
 from tasks import Task, TeamTask
+import db
 
 bot = interactions.Client(token=get_token())
-
-
-# @bot.command()
-# @interactions.option()
-# async def tasklist(ctx: interactions.CommandContext, team: str = None):
-#     """Lists all the current tasks (private/assigned to you)
-#     Args:
-#         channel (interactions.CommandContext): _description_
-#     """
-#     await ctx.send(get_tasks(team) if team else get_tasks(ctx.author.name))
-
 
 if __name__ == "__main__":
     bot.start()
