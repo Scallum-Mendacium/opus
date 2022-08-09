1877a181f19d1ab8dc008cb729f455966940c061
Utshaan <Utkarsh.thanvi@gmail.com>
2022-08-09 22:03:30 +0530

    Added new bot functions && Implemented error handling

--- src/main.py
+++ src/main.py
@@ -24,14 +24,29 @@ async def tasklist(
     """
     if group.lower() == "user":
         if group_name == " ":
-            await ctx.send(compile_format(db.get_user_tasks(ctx.author.name)))
+            try:
+                tasks = db.get_user_tasks(ctx.author.name)
+            except AttributeError:
+                await ctx.send("`You have 0 tasks at the moment`")
+                return
+            await ctx.send(compile_format(tasks))
         else:
-            await ctx.send(compile_format(db.get_user_tasks(group_name)))
+            try:
+                tasks = db.get_user_tasks(group_name)
+            except AttributeError:
+                await ctx.send(f"`{group_name} has 0 tasks at the moment`")
+                return
+            await ctx.send(compile_format(tasks))
     elif group.lower() == "team":
         if group_name == " ":
             await ctx.send("```Error: Please give the team name as an argument```")
         else:
-            await ctx.send(compile_format(db.get_team_tasks(group_name)))
+            try:
+                tasks = db.get_team_tasks(group_name)
+            except AttributeError:
+                await ctx.send(f"`{group_name} has 0 tasks at the moment`")
+                return
+            await ctx.send(compile_format(tasks))
 
 
 @bot.command()
@@ -111,10 +126,49 @@ async def remove_task(
 ):
     if team_name == " ":
         db.delete_user_task_explicit(ctx.author.name, task_name)
-        await ctx.send("`Task deleted`")
     else:
         db.delete_team_task_explicit(team_name, task_name)
-        await ctx.send("`Task deleted`")
+    
+    await ctx.send("`Task deleted`")
+
+@bot.command()
+@interactions.option(name="task_name", description="Name of task", required=True)
+@interactions.option(name="team_name", description="Name of team to move task to", required=True)
+@interactions.option(name="source", description="Group to which the task belongs (leave empty for user tasks)", required=False)
+async def move_to_team(ctx: interactions.CommandContext, task_name: str, team_name: str, source: str = " "):
+    if source == " ":
+        try:
+            task = [task for task in db.get_user_tasks(ctx.author.name) if task.task_name == task_name][0]
+        except IndexError:
+            await ctx.send(f"`There are no tasks matching {task_name}`")
+        db.delete_user_task_implicit(task)
+        db.add_team_task(task.to_team(team_name))
+    else:
+        try:
+            task = [task for task in db.get_team_tasks(source) if task.task_name == task_name][0]
+        except AttributeError:
+            await ctx.send(f"`There is no team named {team_name}`")
+            return
+        except IndexError:
+            await ctx.send(f"`There are no tasks matching {task_name}`")
+            return
+        db.delete_team_task_implicit(task)
+        db.add_team_task(task.to_team(team_name))
+    await ctx.send(f"`Task transfered to {team_name}`")
+
+@bot.command()
+@interactions.option(name="task_name", description="Name of task", required=True)
+@interactions.option(name="source", description="Team to which the task belongs", required=True)
+async def move_to_self(ctx: interactions.CommandContext, task_name: str, source: str):
+    try:
+        task = [task for task in db.get_team_tasks(source) if task.task_name == task_name][0]
+    except AttributeError:
+        await ctx.send(f"`There is no task matching {task_name} in {source}`")
+        return
+    db.delete_team_task_implicit(task)
+    db.add_user_task(task.to_user(ctx.author.name))
+    await ctx.send(f"`Task added to {ctx.author.name}`")
+    
 
 
 if __name__ == "__main__":
